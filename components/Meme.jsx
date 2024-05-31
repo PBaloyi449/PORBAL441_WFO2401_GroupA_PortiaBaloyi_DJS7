b9ad259b9d8caa65942bda369907b08d915637d5
PBaloyi449 <pbaloyi449@gmail.com>
2024-05-31 13:07:13 +0200

    Setting the top text and bottom text

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -4,21 +4,29 @@ export default function Meme() {
     return (
         <main>
             <form className="form">
+                <div>
+                <label htmlFor="top-text">Top text </label>
                 <input 
+                id="top-text"
+                type="text" 
+                placeholder="Shut up"
+                className="form--input"
+                />
+                </div>
+                <div>
+                <label htmlFor="bottom-text">Bottom text </label>
+                <input 
+                id="bottom-text"
                 type="text" 
                 placeholder="Top text"
                 className="form--input"
                 />
-                <input 
-                type="text" 
-                placeholder="Top text"
-                className="form--input"
-                />
+                </div>
                 <button
                     className="form--button"
                 >
                     Get a new meme image 🖼
-                    </button>
+                </button>
             </form>
         </main>
     )
