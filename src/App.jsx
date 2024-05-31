5c668946ceb1dc79c587098dec56585e82251984
PBaloyi449 <pbaloyi449@gmail.com>
2024-05-31 16:07:25 +0200

    Fixing bugs

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@
 //import React from "react"
 import Header from "../components/Header"
 import Meme from "../components/Meme"
-/**
- * Challenge: Build the Header component
- */
+
 export default function App() {
     return (
         <div>
